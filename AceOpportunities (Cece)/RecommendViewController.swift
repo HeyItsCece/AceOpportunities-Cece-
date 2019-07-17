96c05be84f383b2584de9a49c9905e63f8cc62af
Girls Code <GWC@cs81-ms-crt-19.local>
2019-07-17 13:28:33 -0400

    I started the randomizer. Please fix up the string.

---
+++ AceOpportunities (Cece)/RecommendViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RecommendViewController.swift
+//  AceOpportunities (Cece)
+//
+//  Created by Girls Who Code on 7/17/19.
+//  Copyright © 2019 Girls Who Code. All rights reserved.
+//
+
+import UIKit
+
+class RecommendViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var RecommendLabel: UILabel!
+    
+    @IBAction func HandlePickOpportunity(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    var Opportunities: [String] = ["Testing 1", "Testing 2", "Testing 3"]
+    
+    
+}
